ad1a1528389dd3164766494abd71deb30b2c3bdc
Manuel Huber <Manuel.h87@gmail.com>
2012-08-23 11:49:39 +0200

    Added documentation for FixNumberActivity
     * Removed override onStop: didn't do anything...

--- src/org/booncode/android/loco/FixNumberActivity.java
+++ src/org/booncode/android/loco/FixNumberActivity.java
@@ -25,21 +25,46 @@ import android.widget.Toast;
 import android.util.Log;
 import android.widget.Toast;
 
+
+/*! \brief This Activity is used to ensure that numbers in
+ *         \ref StalkerDatabase are in international format.
+ * 
+ *  Since there is no method, to normalize a telephone number 
+ *  I decided to bug the user to format the number.
+ * 
+ *  \todo Find a way to remove this activity and automatically format
+ *        telephone numbers to international format.
+ * */
 public class FixNumberActivity extends Activity
 {
+  //! Intent extra key that holds the number to format.
   public static final String FIX_NUMBER = "number";
+  //! Intent extra key that holds the name of the person to add.
   public static final String FIX_NAME = "name";
   
+  //! TAG to identify log messages from this class.
   protected static final String TAG = "loco.FixNumberActivity";
   
+  //! Database object to add person.
   protected StalkerDatabase m_db;
-  
+  //! Reference to the TextView to show the name of the person to add.
   protected TextView m_txt_name;
+  //! Reference to the EditText control to fix the number.
   protected EditText m_txt_number;
-  
+  //! Name extracted from the Intent extras.
   protected String m_name;
+  //! Original (not international) number extracted from the Intent extras.
   protected String m_number;
   
+  
+  /*! \brief Callback method (Activity), called if a instance
+   *         of this activity has been created.
+   * 
+   *  Sets up references to controls and database connection.
+   * 
+   *  \param savedInstanceState bundle to save extra state info.
+   *         No extra fields have been added to this Bundle.
+   * */
   @Override
   public void onCreate(Bundle savedInstanceState)
   {
@@ -52,6 +77,11 @@ public class FixNumberActivity extends Activity
     m_db = new StalkerDatabase(getApplicationContext());
   }
   
+  /*! Helper method to extract all Intent extras (#FIX_NAME, #FIX_NUMBER).
+   * 
+   *  \return Returns \c true if #m_name and #m_number could be extracted,
+   *          else \c false.
+   * */
   protected boolean extractData()
   {
     Bundle bundle = getIntent().getExtras();
@@ -72,11 +102,26 @@ public class FixNumberActivity extends Activity
     return false;
   }
   
+  /*! Callback method, called if cancel button has been pressed.
+   * 
+   *  \param v The view that has been pressed.
+   * */
   public void onCancel(View v)
   {
     finish();
   }
   
+  /*! Callback method, called if the save button has been pressed.
+   * 
+   *  This method checks if number could be in international format,
+   *  and if \c PhoneNumberUtils think the entered number is
+   *  equal to the original number (#m_number).
+   * 
+   *  \note I'm not sure if \c PhoneNumberUtils.compare really works.
+   *        I have to test it with some numbers...
+   * 
+   *  \param v The view that has been pressed.
+   * */
   public void onSave(View v)
   {
     String number = m_txt_number.getText().toString();
@@ -104,6 +149,11 @@ public class FixNumberActivity extends Activity
     }
   }
   
+  /*! \brief Callback method (Activity), called if Activity got started.
+   * 
+   *  If #extractData return \c false, the Activity will be closed
+   *  immediately.
+   * */
   @Override
   public void onStart()
   {
@@ -114,12 +164,11 @@ public class FixNumberActivity extends Activity
     }
   }
   
-  @Override
-  public void onStop()
-  {
-    super.onStop();
-  }
-  
+  /*! \brief Callback method (Activity), called if Activity is about to
+   *         beeing destroyed.
+   * 
+   *  Closes the database connection.
+   * */
   @Override
   public void onDestroy()
   {
